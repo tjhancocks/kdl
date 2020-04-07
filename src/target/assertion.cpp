17f4ef79dacd34463f1fdd0f6eea8b2c1deb44de
Tom Hancocks <tomhancocks@gmail.com>
2020-04-07 06:48:08 +0100

    Start laying the ground for @type assertions.
    Assertions will be used to help validate certain elements of a resource
    when it is assembled.

---
+++ src/target/assertion.cpp
@@ -0,0 +1,91 @@
+// Copyright (c) 2019-2020 Tom Hancocks
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "diagnostic/fatal.hpp"
+#include "target/assertion.hpp"
+
+// MARK: - Constructor
+
+kdl::assertion::assertion(const lexeme lhs, assertion::operation op, const lexeme rhs)
+    : m_lhs(lhs), m_operation(op), m_rhs(rhs)
+{
+
+}
+
+// MARK: - Evaluation
+
+static auto __resolve_variable(const kdl::lexeme var, std::map<std::string, kdl::lexeme> table) -> kdl::lexeme
+{
+    if (!var.is(kdl::lexeme::var)) {
+        return var;
+    }
+
+    if (table.find(var.text()) == table.end()) {
+        kdl::log::fatal_error(var, 1, "Unknown variable encountered in assertion '" + var.text() + "'");
+    }
+
+    return table.at(var.text());
+}
+
+auto kdl::assertion::evaluate(std::map<std::string, lexeme> variables) const -> bool
+{
+    auto lhs = __resolve_variable(m_lhs, variables);
+    auto rhs = __resolve_variable(m_lhs, variables);
+
+    if (lhs.type() != rhs.type()) {
+        kdl::log::fatal_error(lhs, 1, "Type mismatch in assertion. Both LHS and RHS must be of the same type.");
+    }
+
+    switch (lhs.type()) {
+        case kdl::lexeme::integer:
+        case kdl::lexeme::res_id:
+        case kdl::lexeme::percentage: {
+            // NOTE: This is constraining ourselves to GCC and/or Clang and to 64-bit systems.
+            // Add a fallback in the future.
+            auto v1 = lhs.value<__int128>();
+            auto v2 = rhs.value<__int128>();
+
+            switch (m_operation) {
+                case lt:    return v1 < v2;
+                case lteq:  return v1 <= v2;
+                case eq:    return v1 == v2;
+                case neq:   return v1 != v2;
+                case gteq:  return v1 >= v2;
+                case gt:    return v1 > v2;
+            }
+        };
+
+        case kdl::lexeme::string: {
+            auto v1 = lhs.text();
+            auto v2 = rhs.text();
+            switch (m_operation) {
+                case eq:    return v1 == v2;
+                case neq:   return v1 != v2;
+                default: {
+                    kdl::log::fatal_error(lhs, 1, "Operator not supported for string types.");
+                }
+            }
+        };
+
+        default: {
+            kdl::log::fatal_error(lhs, 1, "Unsupported type found in assertion.");
+        }
+    }
+}
---
+++ src/target/assertion.hpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2019-2020 Tom Hancocks
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#if !defined(KDL_ASSERTION_HPP)
+#define KDL_ASSERTION_HPP
+
+#include <map>
+#include "parser/lexeme.hpp"
+
+namespace kdl
+{
+
+    struct assertion
+    {
+    public:
+        enum operation { lt, lteq, eq, neq, gteq, gt };
+
+    private:
+        lexeme m_lhs;
+        lexeme m_rhs;
+        enum operation m_operation;
+
+    public:
+        assertion(const lexeme lhs, enum operation op, const lexeme rhs);
+
+        auto evaluate(std::map<std::string, lexeme> variables) const -> bool;
+    };
+
+};
+
+#endif //KDL_ASSERTION_HPP
