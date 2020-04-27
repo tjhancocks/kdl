ab5a246c3b096b9aa76e66a8a013022fa3eaad06
Tom Hancocks <tomhancocks@gmail.com>
2020-04-27 19:03:55 +0100

    Change how the decision to output is made.

--- src/main.cpp
+++ src/main.cpp
@@ -120,23 +120,20 @@ auto main(int argc, const char **argv) -> int
         }
     }
 
-    // If there are no input files then end here.
-    if (files.empty()) {
-        return 0;
-    }
+    if (!files.empty()) {
+        // Loop through each of the files and parse them.
+        for (auto file : files) {
+            // 0. Configure the target.
+            target->set_src_root(file->path());
 
-    // Loop through each of the files and parse them.
-    for (auto file : files) {
-        // 0. Configure the target.
-        target->set_src_root(file->path());
+            // 1. Perform lexical analysis.
+            kdl::lexer lexer(file);
+            auto lexemes = lexer.analyze();
 
-        // 1. Perform lexical analysis.
-        kdl::lexer lexer(file);
-        auto lexemes = lexer.analyze();
-
-        // 2. Parse the lexical analysis result.
-        kdl::sema::parser parser(target, lexemes);
-        parser.parse();
+            // 2. Parse the lexical analysis result.
+            kdl::sema::parser parser(target, lexemes);
+            parser.parse();
+        }
     }
 
     // Finally save the target to disk, if there are resources present in it.
