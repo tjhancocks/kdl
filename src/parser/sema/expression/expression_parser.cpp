51833331be2dc19a65087fe63c7a4fc3d952c7b0
Tom Hancocks <tomhancocks@gmail.com>
2022-09-10 08:27:25 +0100

    Existing expression parser should internally use the newer expressions.

--- src/parser/sema/expression/expression_parser.cpp
+++ src/parser/sema/expression/expression_parser.cpp
@@ -21,6 +21,7 @@
 #include <stdexcept>
 #include "diagnostic/fatal.hpp"
 #include "expression_parser.hpp"
+#include "target/new/kdl_expression.hpp"
 
 // MARK: - Constructor
 
@@ -40,114 +41,33 @@ kdl::sema::expression_parser::expression_parser(kdl::sema::parser &parser,
 
 auto kdl::sema::expression_parser::parse() -> kdl::lexeme
 {
-    // Before proceeding with the primary expression parser, we want to try and short circuit
-    // the parse by identifying the variable shorthand.
     if (m_parser.expect({ expectation(lexeme::var).be_true() })) {
-        auto var_name = m_parser.read();
-        return resolve_variable_named(var_name);
+        const auto& var_name = m_parser.read();
+        const auto var = m_target->global_variable(var_name.text());
+        if (var.has_value()) {
+            return var.value();
+        }
+        log::fatal_error(var_name, 1, "Unrecognised variable name encountered in expression.");
+    }
+    else if (m_parser.expect({ expectation(lexeme::l_expr).be_true() })) {
+        // Ensure that we're at the start of an expression.
+        m_parser.ensure({ expectation(lexeme::l_expr).be_true() });
+
+        std::vector<lexeme> expression_lexemes;
+        while (m_parser.expect({ expectation(lexeme::r_expr).be_false() })) {
+            expression_lexemes.emplace_back(m_parser.read());
+        }
+
+        m_parser.ensure({ expectation(lexeme::r_expr).be_true() });
+
+        build_target::kdl_expression expression(expression_lexemes);
+        return expression.evaluate(m_target);
+    }
+    else {
+        log::fatal_error(m_parser.peek(), 1, "Unexpected lexeme encountered in expression.");
     }
 
-    kdl::lexeme result { "?", lexeme::string };
 
-    // Ensure that we're at the start of an expression.
-    m_parser.ensure({ expectation(lexeme::l_expr).be_true() });
-
-    bool is_res_id { false };
-    std::vector<lexeme> output_queue {};
-    std::vector<lexeme> operator_stack {};
-    while (m_parser.expect({ expectation(lexeme::r_expr).be_false() })) {
-        // Read the next token. If it is an identifier then attempt to resolve it as a variable.
-        // TODO: Functions?
-        auto token = m_parser.read();
-        if (token.is(lexeme::identifier)) {
-            token = resolve_variable_named(token);
-        }
-
-        if (token.is(lexeme::res_id)) {
-            is_res_id = true;
-        }
-
-        // Shunting yard...
-        // If any token is a res_id, then the result of the expression will become a res_id.
-        if (token.is(lexeme::integer) || token.is(lexeme::res_id)) {
-            output_queue.emplace_back(token);
-        }
-        else if (token.is(lexeme::plus) || token.is(lexeme::minus) || token.is(lexeme::star) || token.is(lexeme::slash) || token.is(lexeme::carat)) {
-            while (!operator_stack.empty()) {
-                const auto& o2 = operator_stack.back();
-                if ((!o2.is(lexeme::l_paren)) && (o2.value<int64_t>() >= token.value<int64_t>() && token.left_associative())) {
-                    output_queue.emplace_back(operator_stack.back());
-                    operator_stack.pop_back();
-                }
-                else {
-                    break;
-                }
-            }
-            operator_stack.emplace_back(token);
-        }
-        else if (token.is(lexeme::l_paren)) {
-            operator_stack.emplace_back(token);
-        }
-        else if (token.is(lexeme::r_paren)) {
-            while (!operator_stack.empty() && !operator_stack.back().is(lexeme::l_paren)) {
-                output_queue.emplace_back(operator_stack.back());
-                operator_stack.pop_back();
-            }
-
-            if (!operator_stack.back().is(lexeme::l_paren)) {
-                log::fatal_error(operator_stack.back(), 1, "Expected a '(' token.");
-            }
-            operator_stack.pop_back();
-        }
-    }
-
-    while (!operator_stack.empty()) {
-        output_queue.emplace_back(operator_stack.back());
-        operator_stack.pop_back();
-    }
-
-    // Finally ensure that we're actually at the end of the expression.
-    m_parser.ensure({ expectation(lexeme::r_expr).be_true() });
-
-    // We're now ready to evaluate the expression.
-    std::vector<int64_t> working_stack {};
-    for (const auto& o : output_queue) {
-        if (o.is(lexeme::integer) || o.is(lexeme::res_id)) {
-            working_stack.emplace_back(o.value<int64_t>());
-        }
-        else if (o.is(lexeme::plus)) {
-            const auto lhs = working_stack.back(); working_stack.pop_back();
-            const auto rhs = working_stack.back(); working_stack.pop_back();
-            working_stack.emplace_back(lhs + rhs);
-        }
-        else if (o.is(lexeme::minus)) {
-            const auto lhs = working_stack.back(); working_stack.pop_back();
-            const auto rhs = working_stack.back(); working_stack.pop_back();
-            working_stack.emplace_back(lhs - rhs);
-        }
-        else if (o.is(lexeme::star)) {
-            const auto lhs = working_stack.back(); working_stack.pop_back();
-            const auto rhs = working_stack.back(); working_stack.pop_back();
-            working_stack.emplace_back(lhs * rhs);
-        }
-        else if (o.is(lexeme::slash)) {
-            const auto lhs = working_stack.back(); working_stack.pop_back();
-            const auto rhs = working_stack.back(); working_stack.pop_back();
-            working_stack.emplace_back(lhs / rhs);
-        }
-        else if (o.is(lexeme::carat)) {
-            const auto lhs = working_stack.back(); working_stack.pop_back();
-            const auto rhs = working_stack.back(); working_stack.pop_back();
-            working_stack.emplace_back(lhs ^ rhs);
-        }
-    }
-
-    if (working_stack.empty() || working_stack.size() > 1) {
-        throw std::logic_error("There was an error evaluating the expression. Stack size was " + std::to_string(working_stack.size()));
-    }
-    result = kdl::lexeme(std::to_string(working_stack.back()), is_res_id ? lexeme::res_id : lexeme::integer);
-
-    return result;
 }
 
 // MARK: - Variables
